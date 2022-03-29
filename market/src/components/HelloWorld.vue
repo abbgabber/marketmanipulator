929d88f2455293de8b9098b196908c895c9f9803
abbgabber <55151330+abbgabber@users.noreply.github.com>
2022-03-29 18:14:31 +0200

    Update 0.14.5
    Added the ability to change timezone

--- market/src/components/HelloWorld.vue
+++ market/src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <div class="loading"><h1>HELLO WORLD</h1></div> -->
     <div class="loading" v-if="loading">
       <v-progress-circular
         indeterminate
@@ -9,14 +8,24 @@
       ></v-progress-circular>
     </div>
 
-    <div class="select--stock">
-      <h3>Select a stock</h3>
+    <div class="options">
+      <v-select
+        class="options--timezone"
+        v-model="tz"
+        :items="timezoneData"
+        item-text="label"
+        label="Timezone"
+        hint="Select your timezone"
+        return-object
+        outlined
+      ></v-select>
       <v-select
         v-model="stocks"
         :items="items"
         item-text="name"
         item-value="id"
         label="Stocks"
+        hint="Select your stock"
         return-object
         outlined
       ></v-select>
@@ -28,9 +37,9 @@
       :height="this.height"
       ref="tradingVue"
     ></trading-vue>
+
     <div class="data" v-if="apiData.length > 0">
       <h4>Current price: {{ apiData[apiData.length - 1][4] }}</h4>
-      <!-- Price, 7-week change (price + %), 1-year change (price + %), Previous Close  -->
     </div>
   </div>
 </template>
@@ -38,54 +47,9 @@
 import { TradingVue } from "trading-vue-js";
 import { EMA, RSI } from "technicalindicators";
 var axios = require("axios");
+const timezoneData = require("./tz.json");
 
 let data = [];
-
-const trades = [
-  // [1583483331000, 0, 1.1249],
-  // [1583483391000, 0, 1.12554],
-  // [1583483511000, 0, 1.1259],
-  // [1583483571000, 0, 1.12685],
-  // [1583483751000, 0, 1.12714],
-  // [1583483791000, 0, 1.12757],
-  // [1583484831000, 0, 1.12783],
-  // [1583484831000, 0, 1.12783],
-  // [1583487291000, 0, 1.12826],
-  // [1583487291000, 0, 1.12826],
-  // [1583487651000, 0, 1.12852],
-  // [1583487651000, 0, 1.12852],
-  // [1583487711000, 0, 1.12879],
-  // [1583487711000, 0, 1.12879],
-  // [1583487831000, 0, 1.12892],
-  // [1583487831000, 0, 1.12892],
-  // [1583488351000, 0, 1.12908],
-  // [1583488351000, 0, 1.12908],
-  // [1583488671000, 0, 1.12952],
-  // [1583488671000, 0, 1.12952],
-  // [1583489031000, 0, 1.12989],
-  // [1583489031000, 0, 1.12989],
-  // [1583489151000, 0, 1.13054],
-  // [1583489151000, 0, 1.13054],
-  // [1583501820000, 1, 1.13069],
-];
-
-const lines = [];
-// trades.forEach((t, i) => {
-//   if (i === trades.length - 1) {
-//     return;
-//   }
-
-//   lines.push({
-//     name: "line_" + i,
-//     type: "Segment",
-//     data: [],
-//     settings: {
-//       p1: [t[0], t[2]],
-//       p2: [trades[trades.length - 1][0] + 3600000, t[2]],
-//       color: t[1] === 1 ? "green" : "red",
-//     },
-//   });
-// });
 
 export default {
   name: "app",
@@ -101,13 +65,16 @@ export default {
         },
         { name: "IBM", id: "IBM" },
       ],
+      tz: timezoneData[26],
       loading: false,
       stocks: "",
+      timezoneData: timezoneData,
       apiData: [],
       prediction: false,
       titleText: "Market Manipulator2k",
-      width: window.innerWidth - 20,
-      height: window.innerHeight - 20,
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+
       ohlcv: data,
       onchart: [
         {
@@ -134,20 +101,15 @@ export default {
           data: [],
           settings: { color: "orange" },
         },
-        {
-          name: "Trades",
-          type: "Trades",
-          data: trades,
-          settings: {
-            markerSize: 12,
-          },
-        },
-        ...lines,
       ],
       offchart: [{ name: "RSI", type: "RSI", data: [] }],
     };
   },
   methods: {
+    getDimensions() {
+      this.width = document.documentElement.clientWidth;
+      this.height = document.documentElement.clientHeight;
+    },
     async fetchData(id) {
       return await axios.request({
         method: "GET",
@@ -165,39 +127,42 @@ export default {
       )) {
         let date = new Date(timestamp);
         this.apiData.push([
-          date.getTime(),
+          date.getTime() + (parseInt(this.tz.value) + 4) * 60 * 60 * 1000,
           parseFloat(value["1. open"]),
           parseFloat(value["2. high"]),
           parseFloat(value["3. low"]),
           parseFloat(value["4. close"]),
         ]);
       }
+
       this.apiData.reverse();
       this.ohlcv = this.apiData;
-
-      this.prediction = this.candleStickPred();
 
       const closeData = [];
       this.apiData.forEach((d) => closeData.push(d[4]));
 
+      this.onchart[0].data = [];
       const ema9Data = new EMA.calculate({ period: 9, values: closeData });
       this.apiData.forEach((d, i) => {
         const emad = i > 9 ? ema9Data[i - 9] : undefined;
         this.onchart[0].data.push([d[0], emad]);
       });
 
+      this.onchart[1].data = [];
       const ema20Data = new EMA.calculate({ period: 20, values: closeData });
       this.apiData.forEach((d, i) => {
         const emad = i > 20 ? ema20Data[i - 20] : undefined;
         this.onchart[1].data.push([d[0], emad]);
       });
 
+      this.onchart[2].data = [];
       const ema50Data = new EMA.calculate({ period: 50, values: closeData });
       this.apiData.forEach((d, i) => {
         const emad = i > 50 ? ema50Data[i - 50] : undefined;
         this.onchart[2].data.push([d[0], emad]);
       });
 
+      this.onchart[3].data = [];
       const ema200Data = new EMA.calculate({ period: 200, values: closeData });
       this.apiData.forEach((d, i) => {
         const emad = i > 200 ? ema200Data[i - 200] : undefined;
@@ -208,6 +173,7 @@ export default {
         values: closeData,
         period: 14,
       };
+      this.offchart[0].data = [];
       const rsiData = RSI.calculate(inputRSI);
       RSI.calculate(inputRSI);
       this.apiData.forEach((d, i) => {
@@ -217,6 +183,7 @@ export default {
 
       this.$refs.tradingVue.resetChart();
     },
+
     candleStickPred() {
       for (let i = 1; i <= 3; i++) {
         let v =
@@ -238,22 +205,33 @@ export default {
   created() {
     // this.insertData();
   },
-  mounted() {},
+  mounted() {
+    window.addEventListener("resize", this.getDimensions, { passive: true });
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.getDimensions, { passive: true });
+  },
   watch: {
     stocks() {
       this.updateData(this.stocks.id);
+    },
+    tz() {
+      if (this.apiData.length > 0) {
+        this.updateData(this.stocks.id);
+      }
     },
   },
 };
 </script>
 <style>
-.select--stock {
+.options {
   position: absolute;
   z-index: 10;
   left: 85vw;
   top: 2vh;
-  max-width: 10rem;
+  max-width: 11rem;
 }
+
 .loading {
   position: absolute;
   top: 40%;
